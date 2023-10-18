a1cae5e2978d6d47ecc6beb2a725da3906c8cb14
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-18 16:55:32 +0300

    added store

---
+++ resources/views/products.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products</title>
+</head>
+<body>
+    <h1>Products</h1>
+    <main>
+        <article>
+            <h2>Doctor's Sausage</h2>
+            <img src="/images/desa.jpg" alt="tasty sausage">
+            <p>Tasty sausage. When you taste it, you will be amazed.🤤</p>
+            <p>1.49 EUR</p>
+        </article>      
+    </main>
+</body>
+</html>
